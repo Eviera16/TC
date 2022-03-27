1bc7b3b628fd9570bbbf141377fa72e4bcf6e0c1
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-03-27 11:48:03 -0400

    Initial Commit

---
+++ TapTapCoin/TapTapCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapCoinApp.swift
+//  TapTapCoin
+//
+//  Created by Eric Viera on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TapTapCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
